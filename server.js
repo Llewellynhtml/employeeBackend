acd128d76fd5b19d589dee4b3f19a3d9fa92d206
Llewellyn <Llewellyn.ml.info@gmail.com>
2024-12-02 14:38:39 +0200

    Backendemployee

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const dbRoutes = require('./routes/db');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const cors = require("cors");
+const dbRoutes = require("./routes/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/db', dbRoutes); 
-app.use('/api/auth', authRoutes); 
+app.use("/api/db", dbRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   const statusCode = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong!';
+  const errorMessage = err.message || "Something went wrong!";
   res.status(statusCode).json({ error: errorMessage });
 });
 
-
-app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
